6713afa4a015c7b29a727fa1b747c32bc562c902
Fedeper <federiocoperalta@outlook.com>
2017-07-04 21:57:08 -0300

    modelo tarea

--- tareas/admin.py
+++ tareas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria,Tarea,Comentario
+
+
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Tarea)
+admin.site.register(Comentario)
--- tareas/models.py
+++ tareas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Categoria(models.Model):
+    pass
+
+class Tarea(models.Model):
+    URGENTE=10
+    MEDIA = 50
+    BAJA = 100
+    RANGO_DE_PRIORIDAD = ((URGENTE,'Urgente'),(MEDIA,'Medio'),(BAJA,'Baja'))
+    INVALIDO = 'Invalido'
+    EN_CURSO = 'En curso'
+    PENDIENTE = 'Pendiente'
+    FINALIZADO = 'Finalizado'
+    RANGO_DE_ESTADOS = ((INVALIDO,'Invalido'),(EN_CURSO,'En curso'),(PENDIENTE,'Pendiente'),(FINALIZADO,'Finalizado'))
+    usuario = models.ForeignKey(User)
+    categoria = models.ForeignKey('Categoria')
+    prioridad = models.IntegerField(choices=RANGO_DE_PRIORIDAD)
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateField((), auto_now_add = True)
+    estado = models.CharField(max_length=15, choices=RANGO_DE_ESTADOS)
+
+class Comentario(models.Model):
+    pass
+
+
