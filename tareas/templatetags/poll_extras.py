8497b63208d67a95342e41c1fb93e8d9eeac2c7c
Fedeper <federiocoperalta@outlook.com>
2017-10-27 19:34:41 -0300

    link a tareas

--- tareas/templatetags/poll_extras.py
+++ tareas/templatetags/poll_extras.py
@@ -13,18 +13,17 @@ patron = re.compile('#(\d+)')
 def template_filter(texto):
 
     result = re.findall(patron,texto)
+    print(result)
     for match in result:
         try:
             tarea_valida = Tarea.objects.get(id = match)
-
         except Tarea.DoesNotExist:
             tarea_valida = None
 
         if tarea_valida:
-            link = "<a href='/tareas/" + match + "/'>#" + match + "</a>"
+            link = "<a href='/tareas/{0}/'>#{0}</a>".format(match)
             if tarea_valida.esta_finalizado:
                 link = "<del>"+ link + "</del>"
-            else:
-                link = link
-            texto = texto.replace('#' + match, link)
+
+            texto = re.sub('#{0}(?!\d)'.format(match), link, texto)
     return mark_safe(texto)
